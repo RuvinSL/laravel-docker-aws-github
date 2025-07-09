156cab025c2be01ca1e75eed3682adea6359ca8a
Ruvin Roshan <Ruvin@users.noreply.github.com>
2025-07-09 11:00:02 +0530

    Update LogController.php

--- src/app/Http/Controllers/LogController.php
+++ src/app/Http/Controllers/LogController.php
@@ -13,7 +13,7 @@ class LogController extends Controller
      */
     public function index(Request $request)
     {
-        Log::info('LogController index method called! hey this message edited');
+        Log::info('LogController index method called! hey this message edited, this is third msg');
 
 
         return response()->json([
