6add34463bbcc71a08c40677af2e26837a43dc9e
Ruvin Roshan <ruvinroshan@gmail.com>
2025-07-14 17:23:11 +0530

    created login page and test method

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 
 
 Route::get('/log', [LogController::class, 'index'])->name('log.index');
+
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
